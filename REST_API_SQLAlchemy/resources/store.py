5abd921b31ce4b638c5a3fcd56cfb327108a9cdd
Lucas Deschamps <l-deschamps@outlook.com>
2020-03-08 20:32:40 -0300

    SQLAlchemy RESTful API; also contains a store resource, in addition to the item and user ones

---
+++ REST_API_SQLAlchemy/resources/store.py
@@ -0,0 +1,44 @@
+from flask_restful import Resource
+from models.store_m import StoreModel
+
+class Store(Resource):
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+        
+        if store is not None:
+            return store.json()
+
+        return {'message': 'Store not found.'}, 404
+
+    def post(self, name):
+        if StoreModel.find_by_name(name) is not None:
+            return {'message': 'A store with name \'{}\' already exists.'
+                    .format(name)}, 400
+            
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {'message': 'An error occurred while creating the store.'}, 500
+
+        return store.json(), 201
+
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+
+        if store is not None:
+            store.delete_from_db()
+
+        return {'message': 'Store deleted.'}
+
+class StoreList(Resource):
+    def get(self):
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
+
+    def delete(self):
+        store_list = StoreModel.query.all()
+
+        for stores in store_list:
+            stores.delete_from_db()
+
+        return {'message': 'All stores deleted.'}
