bd4a3eca5ea3a78f1e595920abecddca7bee084e
Lucas Deschamps <l-deschamps@outlook.com>
2020-03-10 19:10:17 -0300

    Adding run.py; tweaking files for Heroku startup

--- REST_API_SQLAlchemy/RESTAPI.py
+++ REST_API_SQLAlchemy/RESTAPI.py
@@ -14,10 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'deschamps'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(UserRegister, '/register')
